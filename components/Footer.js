fd216a30a11882c36a4a8e8dab0eb86050c4ad05
Troygit config --global user.email <tydamon@hotmail.com>
2026-04-27 15:30:19 +1000

    styled footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,8 @@
 // components/Footer.js
 'use client';
 
+import { FaGithub } from "react-icons/fa";
+
 export default function Footer() {
     return (
         <footer className="w-full bg-black text-white border-t border-blue-900">
@@ -9,15 +11,19 @@ export default function Footer() {
                 {/* Top line */}
                 <div className="w-full h-[2px] bg-cyan-500 mb-6"></div>
 
-                <p className="text-sm text-gray-400 mb-1">sifzerda
-                    <span>
-                        <a
-                            href="https://github.com/sifzerda/nx-asteroids"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="mb-3 hover:text-gray-400 transition"> 
-                            🚀 </a>
-                    </span>2026</p>
+                <p className="text-sm text-gray-400 flex items-center gap-2">
+                    <span>sifzerda</span>
+
+                    <a
+                        href="https://github.com/sifzerda/nx-asteroids"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="flex items-center gap-1 text-gray-400 hover:text-cyan-300 transition">
+                        <FaGithub className="text-base" />
+                    </a>
+
+                    <span>2026</span>
+                </p>
 
                 {/* Bottom blue line */}
                 <div className="w-full h-[2px] bg-blue-900 mt-6"></div>
